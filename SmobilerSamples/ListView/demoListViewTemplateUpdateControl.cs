aecb5c1969475e7bf9c26bd8fc44cb1f4994a0eb
lula.jin <lula.jin@searching-info.com>
2019-09-03 10:34:31 +0800

    增加ListView模板更新

---
+++ SmobilerSamples/ListView/demoListViewTemplateUpdateControl.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Smobiler.Core;
+using Smobiler.Core.Controls;
+
+namespace SmobilerSamples.ListView
+{
+    ////ToolboxItem用于控制是否添加自定义控件到工具箱，true添加，false不添加
+    //[System.ComponentModel.ToolboxItem(true)]
+    partial class demoListViewTemplateUpdateControl : Smobiler.Core.Controls.MobileUserControl
+    {
+        public demoListViewTemplateUpdateControl() : base()
+        {
+            //This call is required by the SmobilerUserControl.
+            InitializeComponent();
+        }
+
+        private void button1_Press(object sender, EventArgs e)
+        {
+            demoListViewTemplateUpdateForm lp = new demoListViewTemplateUpdateForm(label1.Text, label2.Text);
+            this.Form.Show(lp, (obj, args) =>
+            {
+                if (lp.ShowResult == ShowResult.Yes)
+                {
+                    label1.Text = lp.textBox1.Text;
+                    label2.Text = lp.textBox2.Text;
+                }
+            });
+        }
+    }
+}
---
+++ SmobilerSamples/ListView/demoListViewTemplateUpdateForm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Smobiler.Core;
+using Smobiler.Core.Controls;
+
+namespace SmobilerSamples.ListView
+{
+    partial class demoListViewTemplateUpdateForm : Smobiler.Core.Controls.MobileForm
+    {
+        public demoListViewTemplateUpdateForm() : base()
+        {
+            //This call is required by the SmobilerForm.
+            InitializeComponent();
+        }
+        private string text1;
+        private string text2;
+        public demoListViewTemplateUpdateForm(string textbox1,string textbox2) : base()
+        {
+            text1 = textbox1;
+            text2 = textbox2;
+            //This call is required by the SmobilerForm.
+            InitializeComponent();
+        }
+        private void demoListViewTemplateUpdateForm_Load(object sender, EventArgs e)
+        {
+            this.ShowResult = ShowResult.No;
+            if (!string.IsNullOrEmpty(text1) & !string.IsNullOrEmpty(text2))
+            {
+                textBox1.Text = text1;
+                textBox2.Text = text2;
+            }
+        }
+        private void title1_ImagePress(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Press(object sender, EventArgs e)
+        {
+            this.ShowResult = ShowResult.Yes;
+            this.Close();
+        }
+
+
+    }
+}
