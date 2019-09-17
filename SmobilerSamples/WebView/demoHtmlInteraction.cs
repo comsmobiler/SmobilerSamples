d6a69029824efd28e2d6a6589dae2f25a7c6baf4
peter <peter.pan@searching-info.com>
2019-09-17 17:39:35 +0800

    修改WebView中消息交互

--- SmobilerSamples/WebView/demoHtmlInteraction.cs
+++ SmobilerSamples/WebView/demoHtmlInteraction.cs
@@ -18,65 +18,21 @@ namespace SmobilerSamples.WebView
         {
             //PostMessage用于向网页发送消息，类型为字符串，具体处理详见jsCookie.html中的说明。
             //这里使用json格式，为了便于示例，使用字符串拼接，实际中建议使用Json序列化方式操作。
-            this.webView1.PostMessage(@"var Days = 30;
-    var exp = new Date();
-    exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 1000);
-    document.cookie = 'name=' + escape('smopwd') + ';expires=' + exp.toGMTString();
-     document.getElementById('smoMessage').innerHTML =
-              '从Smobiler接收的消息: ' + 'SetCookie';
-    var result = JSON.stringify({
-      code: 0,
-      message:'',
-      data:'设置成功'
-    });
-    window.ReactNativeWebView.postMessage(result);
-");
+            this.webView1.PostMessage(@"ProcessSmoMessage({""action"":""setCookie"",""data"":{""name"":""smo"",""value"":""smopwd""}});");
         }
 
         private void btnGetCookie_Press(object sender, EventArgs e)
         {
             //PostMessage用于向网页发送消息，类型为字符串，具体处理详见jsCookie.html中的说明。
             //这里使用json格式，为了便于示例，使用字符串拼接，实际中建议使用Json序列化方式操作。
-            this.webView1.PostMessage(@"var arr, reg = new RegExp('(^| )' + 'name' + '=([^;]*)(;|$)');
-    var cookie = null;
-    if (arr = document.cookie.match(reg)) {
-      cookie = unescape(arr[2]);
-    }
-     document.getElementById('smoMessage').innerHTML =
-              '从Smobiler接收的消息: ' + 'GetCookie';
-    var result = JSON.stringify({
-      code: 0,
-      message:'',
-      data:cookie
-    });
-    window.ReactNativeWebView.postMessage(result);
-");
+            this.webView1.PostMessage("ProcessSmoMessage({\"action\":\"getCookie\",\"data\":{\"name\":\"smo\"}});");
         }
 
         private void btnDelCookie_Press(object sender, EventArgs e)
         {
             //PostMessage用于向网页发送消息，类型为字符串，具体处理详见jsCookie.html中的说明。
             //这里使用json格式，为了便于示例，使用字符串拼接，实际中建议使用Json序列化方式操作。
-            this.webView1.PostMessage(@"var exp = new Date();
-    exp.setTime(exp.getTime() - 1);
-            var arr, reg = new RegExp('(^| )' + 'name' + '=([^;]*)(;|$)');
-            var cookie = null;
-     document.getElementById('smoMessage').innerHTML =
-              '从Smobiler接收的消息: ' + 'DelCookie';
-            if (arr = document.cookie.match(reg))
-            {
-                cookie = unescape(arr[2]);
-            }
-            if (cookie != null)
-            {
-                document.cookie = 'name=' + cookie + ';expires=' + exp.toGMTString();
-            }
-            var result = JSON.stringify({
-                code: 0,
-      message: '删除成功'
-            });
-            window.ReactNativeWebView.postMessage(result);
-            ");
+            this.webView1.PostMessage("ProcessSmoMessage({\"action\":\"delCookie\",\"data\":{\"name\":\"smo\"}});");
         }
 
         //此方法用于处理OnMessage事件，此事件会在从网页上向控件发送消息时触发，详见jsCookie.html中的说明。
